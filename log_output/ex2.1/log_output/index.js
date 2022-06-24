144cef898c0fc94badfd8a00b85013897e64ee95
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-06-24 12:17:58 +0300

    Do exercise 2.1

---
+++ log_output/ex2.1/log_output/index.js
@@ -0,0 +1,36 @@
+const crypto = require('node:crypto')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const axios = require('axios')
+const PING_PONG_APP_URL = process.env.PING_APP_URL || 'http://localhost:4000'
+
+const hash = crypto.randomBytes(20).toString('hex');
+let time 
+
+const getTimestamp = () => {
+  return `${time.toISOString()}: ${hash}`
+}
+
+const getOutput = async () => {
+  const timestamp = getTimestamp()
+  const ping_response = await axios.get(`${PING_PONG_APP_URL}/pings`)
+  const pings = ping_response.data.pings
+  return `${timestamp}</br>Ping / Pongs: ${pings}`
+}
+
+const logLoop = async () => {
+  time = new Date()
+  console.log(getTimestamp())
+  setTimeout(logLoop, 5000)
+}
+
+app.get('/', async (req, res) => {
+  res.send(await getOutput())
+})
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`)
+})
+
+logLoop()
