8a1eaaf7b36e15ade5935c1194cda8bdf11d351e
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-06-26 15:04:42 +0300

    Do exercise 2.9

---
+++ project/ex2.9/backend/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { Pool } = require('pg')
+
+const  pool = new Pool({
+  user: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  database: 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+})
+
+const getTodos = async () => {
+  const { rows } = await pool.query('SELECT * FROM todo')
+  return rows || []
+}
+
+const insertTodo = async (todo) => {
+  await pool.query('INSERT INTO todo (todo) VALUES ($1)', [todo])
+}
+
+const initialize = async () => {
+  await pool.query('CREATE TABLE IF NOT EXISTS todo (id SERIAL PRIMARY KEY, todo TEXT)')
+}
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/todos', async (req, res) => {
+  const newTodo = req.body.todo
+  if (!newTodo) {
+    res.sendStatus(400)
+  } else {
+    await insertTodo(newTodo)
+    res.sendStatus(201)
+  }
+})
+
+app.get('/todos', async (req, res) => {
+  const todos = await getTodos()
+  res.json({ todos })
+})
+
+app.listen(port, async () => {
+  console.log(`Server started in port ${port}`)
+  initialize()
+})
