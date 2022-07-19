e14d38ba8a02845125f7c8138988144567c364e2
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-07-19 13:44:38 +0300

    Do exercise 4.2

---
+++ project/v1.7/project/pages/index.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  const initialTodos = []
+  const [todos, setTodos] = useState(initialTodos)
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const todosResponse = await fetch(`/todos`)
+      const todosJson = await todosResponse.json()
+      setTodos(todosJson.todos.map(todo => todo.todo))
+    }
+    fetchData()
+  }, [])
+
+  const handleAddTodo = async (event) => {
+    event.preventDefault()
+    setTodos(currentTodos => currentTodos.concat(input))
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ todo: input })
+    }
+
+    await fetch(`/todos`, options )
+
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Project</title>
+        <meta name="description" content="Project for devops with kubernetes 2022" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image height={500} width={500} src='/api/image.jpg' />
+      <form onSubmit={handleAddTodo} >
+        <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
+        <button type='submit'>add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+    </>
+
+  )
+}
